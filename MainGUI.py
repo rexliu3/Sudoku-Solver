e273d1007cc369039c63ae1ceab8e8d3489e9d60
rexliu3 <40341857+rexliu3@users.noreply.github.com>
2020-05-13 18:21:39 -0700

    AutoComplete working

--- MainGUI.py
+++ MainGUI.py
@@ -18,30 +18,6 @@ class Grid:
                 main[i].append(int(preboard[i][0][j]))
         return main
 
-    '''board = [
-        [7, 8, 0, 4, 0, 0, 1, 2, 0],
-        [6, 0, 0, 0, 7, 5, 0, 0, 9],
-        [0, 0, 0, 6, 0, 1, 0, 7, 8],
-        [0, 0, 7, 0, 4, 0, 2, 6, 0],
-        [0, 0, 1, 0, 5, 0, 9, 3, 0],
-        [9, 0, 4, 0, 6, 0, 0, 0, 5],
-        [0, 7, 0, 3, 0, 0, 0, 1, 2],
-        [1, 2, 0, 0, 0, 7, 4, 0, 0],
-        [0, 4, 9, 2, 0, 6, 0, 0, 7]
-    ]'''
-
-    '''board = [
-        [0, 7, 0, 0, 0, 4, 5, 0, 6],
-        [9, 0, 6, 7, 0, 2, 4, 3, 8],
-        [3, 4, 8, 9, 0, 6, 7, 0, 2],
-        [8, 9, 0, 0, 0, 0, 2, 0, 1],
-        [6, 1, 7, 0, 0, 8, 3, 5, 4],
-        [5, 0, 3, 6, 4, 1, 0, 0, 9],
-        [1, 6, 0, 0, 0, 7, 0, 2, 0],
-        [0, 0, 0, 0, 6, 0, 0, 4, 0],
-        [4, 3, 9, 0, 2, 5, 6, 8, 7]
-    ]'''
-
     pre_board = [
         ["000004050"],
         ["250000079"],
@@ -56,6 +32,7 @@ class Grid:
     ]
 
     board = format_board(pre_board)
+
 
     def __init__(self, rows, columns, width, height):
         self.rows = rows
@@ -132,6 +109,43 @@ class Grid:
                     return False
         return True
 
+    def solve_visual(self, window, time, wrong):
+        # Recursive function to solve Sudoku Board
+        run = True
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        find = find_empty(self.model)
+        if not find or run == False:
+            return True
+        else:
+            row, column = find
+            self.select(row, column, window)
+
+        for k in range(1, 10):
+            # Check if Board with value of k is valid
+            self.sketch(k)
+            update(self, window, time, wrong)
+
+            if is_valid(self.model, k, row, column):
+                # If the Board is valid, place the value k at that spot
+                self.model[row][column] = k
+                self.cubes[row][column].set_value(k)
+                self.cubes[row][column].draw(window)
+                update(self, window, time, wrong)
+                pygame.time.delay(50)
+
+                if self.solve_visual(window, time, wrong):
+                    return True
+
+                self.model[row][column] = 0
+                self.cubes[row][column].set_value(0)
+                update(self, window, time, wrong)
+                self.cubes[row][column].draw(window)
+                pygame.time.delay(50)
+        return False
+
+
 
 class Cube:
     rows = 9
@@ -199,36 +213,11 @@ def format_time(seconds):
     return timeFormatted
 
 
-def solve_visual(inputBoard, window, time, wrong):
-    # Recursive function to solve Sudoku Board
-    find = find_empty(inputBoard.model)
-    row, column = find
-    inputBoard.select(row, column, window)
-
-    for k in range(1, 10):
-        # Check if Board with value of k is valid
-        inputBoard.sketch(k)
-        update(inputBoard, window, time, wrong)
-
-        if is_valid(inputBoard.model, k, row, column):
-            # If the Board is valid, place the value k at that spot
-            inputBoard.place(k)
-            update(inputBoard, window, time, wrong)
-
-
-def solving_visual(inputBoard, window, time, wrong):
-    run = True
-    while not inputBoard.completed_sudoku() and run:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-        solve_visual(inputBoard, window, time, wrong)
-
-
 def update(inputBoard, window, time, wrong):
+    inputBoard.update_model()
     redraw_window(window, inputBoard, time, wrong)
     pygame.display.update()
-    inputBoard.update_model()
+
 
 
 def main():
@@ -269,7 +258,7 @@ def main():
                     board.clear()
                     key = None
                 if event.key == pygame.K_SPACE:
-                    solving_visual(board, window, play_time, wrong)
+                    board.solve_visual(window, play_time, wrong)
                     key = None
                 if event.key == pygame.K_RETURN:
                     i, j = board.selected
